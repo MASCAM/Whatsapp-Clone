07dcd85fee268d82f52ffb10312d8ca94c840ba0
joaovitor <jvarantescabraltrab@gmail.com>
2020-06-02 16:42:26 -0300

    início da implementação de emojis nas mensagens, timer de gravação do microfone funcionando, ínicio da implementação das mensagens

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -2,9 +2,7 @@ class WhatsAppController {
 
     constructor() {
 
-        console.log('WhatsAppController OK');
-
-        //carrega 
+        //carrega os métodos customizados para os elementos do documento
         this.elementsPrototype();
 
         //carrega os id's dos elementos do document
@@ -170,8 +168,204 @@ class WhatsAppController {
             let formData = new FormData(this.el.formPanelAddContact);
 
         });
+        this.el.contactsMessagesList.querySelectorAll('.contact-item').forEach(item => { //para cada contato na lista de contatos
+
+            item.on('click', e => { //se clicar em um contato
+
+                this.el.home.hide(); //para esconder a tela inicial
+                this.el.main.css({ //para mostrar a conversa com o contato
+
+                    display: 'flex',
+
+                });
+
+            });
+
+        });
+        this.el.btnAttach.on('click', e => {
+
+            e.stopPropagation();
+            this.el.menuAttach.addClass('open'); //para abrir o menu de anexar arquivos
+            document.addEventListener('click', this.closeMenuAttach.bind(this));
+
+        });
+        this.el.btnAttachPhoto.on('click', e=> {
+
+            this.el.inputPhoto.click();
+
+        });
+        this.el.inputPhoto.on('change', e => {
+
+            [...this.el.inputPhoto.files].forEach(file => {
+
+                
+
+            });
+
+        });
+        this.el.btnAttachCamera.on('click', e => {
+
+            this.closeAllMainPanel();
+            this.el.panelCamera.addClass('open');
+            this.el.panelCamera.css({
+
+                'height': 'calc(100%)', 
+
+            });
+
+        });
+        this.el.btnClosePanelCamera.on('click', e => {
+
+            this.closeAllMainPanel();
+            this.el.panelMessagesContainer.show();
+
+        });
+        this.el.btnTakePicture.on('click', e => {
+
+            console.log('tirar foto');
+
+        });
+        this.el.btnAttachDocument.on('click', e => {
+
+            this.closeAllMainPanel();
+            this.el.panelDocumentPreview.addClass('open');
+            this.el.panelDocumentPreview.css({
+
+                'height': 'calc(100%)', 
+
+            });
+
+        });
+        this.el.btnClosePanelDocumentPreview.on('click', e => {
+
+            this.closeAllMainPanel();
+            this.el.panelMessagesContainer.show();
+
+        });
+        this.el.btnSendDocument.on('click', e => {
+
+            console.log('enviar documento');
+
+        });
+        this.el.btnAttachContact.on('click', e => {
+
+            this.el.modalContacts.show();
+
+        });
+        this.el.btnCloseModalContacts.on('click', e => {
+
+            this.el.modalContacts.hide();
+
+        });
+        this.el.btnSendMicrophone.on('click', e => {
+
+            this.el.recordMicrophone.show();
+            this.el.btnSendMicrophone.hide();
+            this.startRecordMicrophoneTime();
+
+        });
+        this.el.btnCancelMicrophone.on('click', e => {
+
+            this.closeRecordMicrophone();
+
+        });
+        this.el.btnFinishMicrophone.on('click', e => {
+
+            this.closeRecordMicrophone();
+
+        });
+        this.el.inputText.on('keypress', e => { //se não estiver com a tecla ctrl pressionada ao pressionar enter, envia a mensagem
+
+            if (e.key === 'Enter' && !e.shiftKey) {
+
+                e.preventDefault();
+                this.el.btnSend.click();
+
+            }
+
+        });
+        this.el.inputText.on('keyup', e => {
+
+            if (this.el.inputText.innerHTML.length > 0) {
+
+                this.el.inputPlaceholder.hide(); //para esconder o texto 'digite uma mensagem'
+                this.el.btnSendMicrophone.hide();
+                this.el.btnSend.show();
+
+            } else {
+
+                this.el.btnSend.hide();
+                this.el.inputPlaceholder.show();
+                this.el.btnSendMicrophone.show();
+
+            }
+
+        });
+        this.el.btnSend.on('click', e => {
+
+            console.log(this.el.inputText.innerHTML);
+
+        });
+        this.el.btnEmojis.on('click', e => {
+
+            this.el.panelEmojis.toggleClass('open');
+
+        });
+        this.el.panelEmojis.querySelectorAll('.emojik').forEach(emoji => {
+
+            emoji.on('click', e => { //código para inserir emoji na mensagem
+
+                let img = this.el.imgEmojiDefault.cloneNode();
+                img.style.cssText = emoji.style.cssText;
+                img.dataset.unicode = emoji.dataset.unicode;
+                img.alt = emoji.dataset.unicode;
+                emoji.classList.forEach(name => {
+
+                    img.addClass(name);
+
+                });
+                this.el.inputText.appendChild(img);
+                this.el.inputText.dispatchEvent(new Event('keyup')); //para esconder o texto de fundo
+
+            });
+
+        });
 
     } //fechando o initEvents()
+
+    startRecordMicrophoneTime() { //método para iniciar a medida do tempo de gravação
+
+        let start = Date.now();
+        this._recordMicrophoneInterval = setInterval(() => {
+
+            this.el.recordMicrophoneTimer.innerHTML = Format.toTime(Date.now() - start);
+
+        }, 100);
+
+    } //fechando o startRecordMicrophoneTime()
+
+    closeRecordMicrophone() { //método para finalizar a gravação do microfone
+
+        this.el.recordMicrophone.hide();
+        this.el.btnSendMicrophone.show();
+        clearInterval(this._recordMicrophoneInterval);
+
+    } //fechando o closeRecordMicrophone()
+
+    closeAllMainPanel() { //para fechar todos os paineis na conversa
+
+        this.el.panelMessagesContainer.hide();
+        this.el.panelDocumentPreview.removeClass('open');
+        this.el.panelCamera.removeClass('open');
+
+    } //fechando o closeAllMainPanel()
+
+    closeMenuAttach(e) { //função para fechar o menu de anexar arquivos
+
+        document.removeEventListener('click', this.closeMenuAttach);
+        this.el.menuAttach.removeClass('open');
+
+    } //fechando o closeMenuAttach()
 
     closeAllLeftPanels() { //para fechar os painéis do lado esquerdo
 
